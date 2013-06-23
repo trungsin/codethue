f726a967d50a69cfe6b93b110c47f1d36b8fd241
sin.trunganh@gmail.com <sin.trunganh@gmail.com@24558267-b3a4-29be-2535-a60b671f1d8f>
2013-06-23 16:40:36 +0000

    ! add facebook

--- trunk/protected/config/main.php
+++ trunk/protected/config/main.php
@@ -10,7 +10,7 @@ Yii::setPathOfAlias('bootstrap', dirname(__FILE__).'/../extensions/bootstrap');
 
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Code Thuê ',
+	'name'=>'CodeThue',
 	'theme'=>'bootstrap',
 	
 	// preloading 'log' component
@@ -154,6 +154,28 @@ return array(
 				),
 				*/
 			),
+		),
+		'facebook'=>array(
+			'class' => 'ext.yii-facebook-opengraph.SFacebook',
+			'appId'=>'186070978225694', // needed for JS SDK, Social Plugins and PHP SDK
+			'secret'=>'cc3136af8c3c5389cccc5968c0db92c0', // needed for the PHP SDK
+			//'fileUpload'=>false, // needed to support API POST requests which send files
+			//'trustForwarded'=>false, // trust HTTP_X_FORWARDED_* headers ?
+			//'locale'=>'en_US', // override locale setting (defaults to en_US)
+			//'jsSdk'=>true, // don't include JS SDK
+			//'async'=>true, // load JS SDK asynchronously
+			//'jsCallback'=>false, // declare if you are going to be inserting any JS callbacks to the async JS SDK loader
+			//'status'=>true, // JS SDK - check login status
+			//'cookie'=>true, // JS SDK - enable cookies to allow the server to access the session
+			//'oauth'=>true,  // JS SDK - enable OAuth 2.0
+			//'xfbml'=>true,  // JS SDK - parse XFBML / html5 Social Plugins
+			//'frictionlessRequests'=>true, // JS SDK - enable frictionless requests for request dialogs
+			//'html5'=>true,  // use html5 Social Plugins instead of XFBML
+			//'ogTags'=>array(  // set default OG tags
+				//'title'=>'MY_WEBSITE_NAME',
+				//'description'=>'MY_WEBSITE_DESCRIPTION',
+				//'image'=>'URL_TO_WEBSITE_LOGO',
+			//),
 		),
 	),
 
